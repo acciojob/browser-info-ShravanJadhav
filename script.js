4b06b773a784c6552f05d2e18ffe0a20d1c896dc
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-05-22 11:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const element = document.createElement("p");
+element.innerText = `You are using ${window.navigator.appName} version ${window.navigator.appVersion}`;
+
+document.body.append(element);
